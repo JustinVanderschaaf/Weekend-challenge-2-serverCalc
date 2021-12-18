ec717dda77952adfb085ec3f91bfd9c77bdbdeda
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2021-12-18 15:33:33 -0600

    sending total var over from server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,85 +1,84 @@
 $(document).ready(onReady);
 
 function onReady() {
-    console.log(`So damn ready 🐷`);
+  console.log(`So damn ready 🐷`);
 
-    $('button').on('click', function(evt){
-        evt.preventDefault();
-        operator = $(this).text();
-         console.log(operator); // print the value of i in the console
-        });
-    $('#calculatorForm').on('submit', addCalculation);
-    refresh();
-}
-let operator
-
-function addCalculation(evt) {
-    // Don't reload the page!
+  $("button").on("click", function (evt) {
     evt.preventDefault();
-    //---making a request doc and sending it to the server----
-    // Prepare our  object
-    // to POST to the server
-    // making the inputs into a object to get ready to post to the DOM
-    let equation = {
-        firstNum: $('#firstInput').val(),
-        secondNum: $('#secondInput').val(),
-        operators: operator
-    }
-    console.log('equation',equation);
-
-    // Send data to server-----1b
-    $.ajax({
-        method: 'POST',
-        url: '/equation',
-        // Send the equation to the server
-        // in the request "body"
-        // data: equation will become data: req.body
-        // on the server
-        data: equation
-    })
-        .then((response) => {
-            console.log('POST response', response);
-
-            // Refresh...
-            // GET /answers from the server again
-            // and render to the DOM
-            refresh();
-        })
+    operator = $(this).text();
+    console.log(operator); // print the value of i in the console
+  });
+  $("#calculatorForm").on("submit", addCalculation);
+  refresh();
+  
+  
 }
+let operator;
 
+function addCalculation(event) {
+  // Don't reload the page!
+  event.preventDefault();
+  //---making a request doc and sending it to the server----
+  // Prepare our  object
+  // to POST to the server
+  // making the inputs into a object to get ready to post to the DOM
+  let equation = {
+    firstNum: $("#firstInput").val(),
+    secondNum: $("#secondInput").val(),
+    operators: operator,
+  };
+  console.log("equation", equation);
 
+  // Send data to server-----1b
+  $.ajax({
+    method: "POST",
+    url: "/equation",
+    // Send the equation to the server
+    // in the request "body"
+    // data: equation will become data: req.body
+    // on the server
+    data: equation,
+  }).then((response) => {
+    console.log("POST response", response);
+
+    // Refresh...
+    // GET /answers from the server again
+    // and render to the DOM
+    refresh();
+  });
+  total()
+}
 
 function refresh() {
-    // Make a network request
-    // Make a HTTP request
-    // Make an AJAX request
-    // AJAX === "Asynchronous Javscript and XML"
-    //1a-----goes to app.get answers looks for the send 
-    //1a----and returns
-    let ajaxOptions = {
-        method: 'GET',
-        url: '/answers'
-    };
-    $.ajax(ajaxOptions)
-        .then((response) => {
-            console.log('AJAX request complete!', response);
-            render(response);
-            //3a-----response became the obj array and called
-            //3a-----render with response as an argument
-        });
+  // Make a network request
+  // Make a HTTP request
+  // Make an AJAX request
+  // AJAX === "Asynchronous Javscript and XML"
+  //1a-----goes to app.get answers looks for the send
+  //1a----and returns
+  let ajaxOptions = {
+    method: "GET",
+    url: "/answers",
+  };
+  $.ajax(ajaxOptions).then((response) => {
+    console.log("AJAX request complete!", response);
+    render(response);
+    //3a-----response became the obj array and called
+    //3a-----render with response as an argument
+  });
 
-    console.log(`
+  console.log(`
         Made a network request, but 
         no one has time to wait 
         for that....
-    `)  
+    `);
 }
 
 function render(answers) {
-    // Do some jQuery to render comments (state) to the DOM
-    $('#displayCalc').empty();
-    for (let result of answers) {
-        $('#displayCalc').append(`
+  // Do some jQuery to render comments (state) to the DOM
+  $("#displayCalc").empty();
+  for (let result of answers) {
+    $("#displayCalc").append(`
             <li>
                 ${result.firstNum} ${result.operators}
                 ${result.secondNum} =
@@ -87,6 +86,31 @@ function render(answers) {
                 
                 </div>
             </li>
-        `)
-    }
+        `);
+  }
+}
+
+function totalRender(total){
+    $("#displayResult").text(`${total}`)
+}
+
+function total() {
+
+
+  let ajax = {
+    method: "GET",
+    url: "/result",
+  };
+  $.ajax(ajax).then((response) => {
+    console.log("AJAX request complete!", response);
+    totalRender(response);
+    //3a-----response became the obj array and called
+    //3a-----render with response as an argument
+  });
+
+  console.log(`
+        Made a network request, but 
+        no one has time to wait 
+        for that....
+    `);
 }
--- server/server.js
+++ server/server.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
-let answers = [
-    {firstNum:24,
-    secondNum:12,
-    operators:'+'
-}
-];
-
-
+let answers = [{ firstNum: 24, secondNum: 12, operators: "+" }];
+let result = "8"
 // GET /answers endpoint
 // localhost:5000/answers
-//2a-----coming to app.get /answers to get 
+//2a-----coming to app.get /answers to get
 //2a------data and send the obj array
-app.get('/answers', (req, res) => {
-    console.log('in GET /answers');
-    res.send(answers);
+app.get("/answers", (req, res) => {
+  console.log("in GET /answers");
+  res.send(answers);
 });
+//------
+app.get("/result", (req, res) => {
+    console.log("in GET /result");
+    res.send(result);
+  });
+
+
+
 
 // --------------2b
-app.post('/equation', (req, res) => {
-    console.log('in POST /equation', req.body);
-    answers.push(req.body);
+app.post("/equation", (req, res) => {
+  console.log("in POST /equation", req.body);
+  answers.push(req.body);
 
-    // Send back a status code saying ok code 200(ok) 201(created)👍
-    res.sendStatus(201);
+  // Send back a status code saying ok code 200(ok) 201(created)👍
+  res.sendStatus(201);
 });
-
-
 
 const port = 5000;
 app.listen(port, () => {
-    console.log('I\'m listening 👂')
+  console.log("I'm listening 👂");
 });
